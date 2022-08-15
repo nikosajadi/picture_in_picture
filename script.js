eed12e5a1e5ca40277d471d688188ded9b8af8c7
nikosajadi <na.sajadii.29@gmail.com>
2022-08-15 21:55:40 +0200

    button design

---
+++ script.js
@@ -0,0 +1,14 @@
+const videoElement = document.getElementById('video');
+const button = document.getElement('button');
+
+//Prompt to select media stream, pass to video element, then play 
+async function selectMediaStream(){
+    try{
+
+    }catch(error) {
+        //atch Error Here
+        console.log('whoops, error here:', error);
+    }
+}
+//on Load
+selectMediaStream();
