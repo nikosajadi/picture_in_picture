1eea33283eadcc146abd43309aa369133213451d
nikosajadi <na.sajadii.29@gmail.com>
2022-12-22 22:30:28 +0100

    screen capture API

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const videoElement = document.getElementById("video");
-const button = document.getElementById("button");
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
 
 //Prompt to select media stream, pass to video element, then play
 async function selectMediaStream() {
   try {
+    // capture live screen constent 
+    // https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture
+    //in this part I going setting a constant that is going to be our media stream data and then we waiting to assign that until the user has actually selected which screen or window they want to share
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
     videoElement.srcObject = mediaStream;
+    // when the user clicks and loads the video ,it is going to call a function that going to play the video
     videoElement.onloadedmetadata = () => {
       videoElement.play();
     };
+    //catch statment: to cache any arrors within the consol.log
   } catch (error) {
-    //atch Error Here
+    //catch our Error Here
     console.log("whoops, error here:", error);
   }
 }
